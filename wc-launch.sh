99a459d47d23db18a9e38f0aced63015c14aa1a5
Stephen Roller <roller@cs.utexas.edu>
2013-11-13 13:53:16 -0600

    Launch script for word count.

---
+++ wc-launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar \
+    -D mapred.reduce.tasks=56 \
+    -input $1 \
+    -output "$1-wc" \
+    -mapper wc-map.py \
+    -reducer wc-reduce.py \
+    -combiner wc-reduce.py \
+    -file wc-map.py \
+    -file wc-reduce.py
+
+    #-D stream.num.map.output.key.fields=2 \
+    #-D mapred.text.key.partitioner.options=-k1 \
