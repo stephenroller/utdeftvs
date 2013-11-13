aed57a6e3a55263337de8cdc2546e183612e521b
Stephen Roller <roller@cs.utexas.edu>
2013-11-13 13:58:40 -0600

    contentwords.py doesn't need to worry about counts.

--- contentwords.py
+++ contentwords.py
@@ -1,10 +1,7 @@
-
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import sys
 import argparse
-import logging
-from collections import Counter
 
 GOOD_POS = dict(JJ="a", NN="n", NP="p", VV="v")
 
@@ -26,23 +23,12 @@ def main():
     parser = argparse.ArgumentParser(
                 description='Filters non-content words from a given vs corpus.')
     parser.add_argument('--input', '-i', metavar='[FILE|-]', help='Input file')
-    parser.add_argument('--min', '-m', type=int, help='Minimum word count.', default=50)
     args = parser.parse_args()
     inpt = open(args.input)
 
-    wc = Counter()
-    logging.info("Pass 1...")
-    for sentence in inpt:
-        sentence = [is_content_word(wp) for wp in sentence.strip().split(" ")]
-        sentence = [w for w in sentence if w]
-        wc.update(sentence)
-
     inpt.seek(0)
-    logging.info("Pass 2...")
     for sentence in inpt:
         sentence = [is_content_word(wp) for wp in sentence.strip().split(" ")]
-        sentence = [w for w in sentence if w and wc[w] >= args.min]
-
         if len(sentence) >= 2:
             print " ".join(sentence)
 
