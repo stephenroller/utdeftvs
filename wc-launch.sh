906679adc0847deebdcbe77a3fc99ae109cbd608
Stephen Roller <roller@cs.utexas.edu>
2013-11-21 13:37:17 -0600

    Update wordcount stuff.

--- wc-launch.sh
+++ wc-launch.sh
@@ -3,12 +3,12 @@
 hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar \
     -D mapred.reduce.tasks=56 \
     -input $1 \
-    -output "$1-wc" \
+    -output "$1.wordcount" \
     -mapper wc-map.py \
     -reducer wc-reduce.py \
-    -combiner wc-reduce.py \
     -file wc-map.py \
     -file wc-reduce.py
 
     #-D stream.num.map.output.key.fields=2 \
     #-D mapred.text.key.partitioner.options=-k1 \
+    #-combiner wc-reduce.py \
--- wc-map.py
+++ wc-map.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python2.7
 
 import sys
+import argparse
+from collections import Counter
+from util import pos, bigrams
 
-for line in sys.stdin:
-    line = line.strip().split(" ")
-    for w in line:
-        print "%s\t1" % w
+def main():
+    parser = argparse.ArgumentParser(description='Wordcount mapper')
+    parser.add_argument('--input', '-i', metavar='[FILE|-]', help='Input vector space', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('--contexts', '-c', metavar='FILE', type=argparse.FileType('r'), help='File of contexts (or counts w/ counts)', default='contexts.txt')
+    parser.add_argument('--adjnoun', action='store_true', help='Compute vectors for adj-nouns.')
+    args = parser.parse_args()
+
+    wc = Counter()
+    for line in args.input:
+        line = line.strip().split(" ")
+        if not args.adjnoun:
+            # regular count
+            wc.update(line)
+        else:
+            # adjnoun bigram count
+            adjnouns = ("%s__%s" % (l, r) for i, l, r in bigrams(line) if pos(l) == "a" and pos(r) == "n")
+            wc.update(adjnouns)
+
+    for k, v in wc.iteritems():
+        print "%s\t%d" % (k, v)
+
+
+if __name__ == '__main__':
+    main()
 
