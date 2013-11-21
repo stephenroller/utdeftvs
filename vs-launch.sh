20f891537b1b93a6d05d065551741f65ff7c7b80
Stephen Roller <roller@cs.utexas.edu>
2013-11-21 13:31:53 -0600

    Don't need so many reducers for the vectorspace tasks.

--- vs-launch.sh
+++ vs-launch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar \
-    -D mapred.reduce.tasks=56 \
+    -D mapred.reduce.tasks=32 \
     -D stream.num.map.output.key.fields=2 \
     -D mapred.text.key.partitioner.options=-k1 \
     -input $1 \
-    -output "$1-vs" \
+    -output "$1.coocc" \
     -mapper vs-map.py \
     -reducer vs-reduce.py \
     -file vs-map.py \
