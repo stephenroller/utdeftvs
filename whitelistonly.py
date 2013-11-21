87ea3244e49a31243a901263af9572f4675a6090
Stephen Roller <roller@cs.utexas.edu>
2013-11-21 13:33:58 -0600

    Whitelist files can be word <tab> count files now (convenient for other uses)

--- whitelistonly.py
+++ whitelistonly.py
@@ -6,11 +6,11 @@ import logging
 def main():
     parser = argparse.ArgumentParser(
                 description='Keeps only words from a given list.')
-    parser.add_argument('--input', '-i', metavar='[FILE|-]', help='Input file')
-    parser.add_argument('--whitelist', '-w', type=argparse.FileType('r'), help='Input file')
+    parser.add_argument('--input', '-i', metavar='[FILE|-]', help='Input file', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('--whitelist', '-w', metavar='FILE', type=argparse.FileType('r'), help='Whitelist file')
     args = parser.parse_args()
     inpt = args.input
-    whitelist = set(l.strip() for l in args.whitelist)
+    whitelist = set(l.strip().split("\t")[0] for l in args.whitelist)
 
     for sentence in inpt:
         sentence = [w for w in sentence.strip().split(" ") if w in whitelist]
