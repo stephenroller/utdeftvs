e401f389bb0749f899cde9ac94346e1765b8323b
Stephen Roller <roller@cs.utexas.edu>
2013-11-21 13:35:37 -0600

    Launch script for contentwords.py

---
+++ cw-launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar \
+    -D mapred.reduce.tasks=0 \
+    -input $1 \
+    -output "$1.content" \
+    -mapper contentwords.py \
+    -file contentwords.py
