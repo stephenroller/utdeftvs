3b500e2d38c245a56a3291bd2e83df49a2073f18
Stephen Roller <roller@cs.utexas.edu>
2014-02-11 17:04:07 -0600

    Add support for filtering stopwords.

--- contentwords.py
+++ contentwords.py
@@ -23,12 +23,22 @@ def main():
     parser = argparse.ArgumentParser(
                 description='Filters non-content words from a given vs corpus.')
     parser.add_argument('--input', '-i', metavar='[FILE|-]', type=argparse.FileType('r'), help='Input file', default=sys.stdin)
+    parser.add_argument('--usingpos', '-p', action='store_true', help='Filter content words using the pos tags.')
+    parser.add_argument('--stopwords', '-s', metavar='FILE', type=argparse.FileType('r'), help='Stopwords file.')
     args = parser.parse_args()
+
+    stopwords = set()
+    if args.stopwords:
+        stopwords = set(l.strip().split("\t")[0] for l in args.stopwords)
+
     inpt = args.input
 
     for sentence in inpt:
         sentence = sentence.strip().split(" ")
-        sentence = (is_content_word(wp) for wp in sentence)
+        if args.usingpos:
+            sentence = (is_content_word(wp) for wp in sentence)
+        if stopwords:
+            sentence = (wp for wp in sentence if wp.lower() not in stopwords)
         sentence = [w for w in sentence if w]
         if len(sentence) >= 2:
             print " ".join(sentence)
