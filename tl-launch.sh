6f2c617b9c25391de4a6feeb7f155db065339a31
Stephen Roller <roller@cs.utexas.edu>
2015-05-15 11:02:07 -0500

    Major cleanup.

--- tl-launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-hadoop jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-0.20.2-cdh3u2.jar \
-    -D mapred.reduce.tasks=0 \
-    -input $1 \
-    -output "$1-lp" \
-    -mapper tlp2lemmapos.py \
-    -file tlp2lemmapos.py
-    -file util.py \
--- vs-reduce.py
+++ vs-reduce.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+from collections import defaultdict
 from itertools import groupby
 
 def lines_to_keyval(stream):
@@ -25,24 +26,51 @@ def memory_grouper(keyvalstream):
         memory[key] = memory.get(key, 0) + val
     return ((k, [(k, v)]) for k, v in memory.iteritems())
 
+def memory_grouper_sort(keyvalstream):
+    memory = defaultdict(int)
+    for key, val in keyvalstream:
+        memory[key] += val
+    keys = memory.keys()
+    keys.sort()
+    return ((k, [(k, memory[k])]) for k in keys)
+
+def memory_grouper_sort_val(keyvalstream):
+    memory = defaultdict(int)
+    for key, val in keyvalstream:
+        memory[key] += val
+    keys = memory.keys()
+    keys.sort(key=lambda x: -memory[x])
+    return ((k, [(k, memory[k])]) for k in keys)
+
+
+
 def main():
     parser = argparse.ArgumentParser(
         'Sums items with the same key. The items must be sorted with by the key, and the summed value must be the last column.')
     parser.add_argument('--input', '-i', metavar='[FILE|-]', help='Input vector space', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('--memory', action='store_true')
+    parser.add_argument('--memory', action='store_true', help='Tally up the counts in memory. May cause memory issues.')
+    parser.add_argument('--sort', action='store_true', help='Produce output sorted by key. May be very slow.')
+    parser.add_argument('--sortval', action='store_true', help='Produce output sorted by key. May be very slow.')
     args = parser.parse_args()
 
+
     if args.memory:
-        grouped = memory_grouper(lines_to_keyval(args.input))
+        if args.sort:
+            grouped = memory_grouper_sort(lines_to_keyval(args.input))
+        elif args.sortval:
+            grouped = memory_grouper_sort_val(lines_to_keyval(args.input))
+        else:
+            grouped = memory_grouper(lines_to_keyval(args.input))
     else:
         grouped = groupby(lines_to_keyval(args.input), key=lambda x: x[0])
-
 
     # group items with the same key
     for key, group in grouped:
         # sum up all their values
         vs = sum(v for k, v in group)
         # and output the summed pair
+        if int(vs) == vs:
+            vs = int(vs)
         if isinstance(vs, int):
             print "%s\t%d" % (key, vs)
         else:
