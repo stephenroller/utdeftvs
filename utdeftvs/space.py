d39e635fc2b0668ac20be2a3b449ea8bae068f68
Stephen Roller <roller@cs.utexas.edu>
2015-10-08 15:16:17 -0500

    Better handle saving when there's a context matrix.

--- utdeftvs/space.py
+++ utdeftvs/space.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 import numpy as np
+import struct
 from sklearn.preprocessing import normalize
 
 class VectorSpace(object):
-    def __init__(self, matrix, vocab):
+    def __init__(self, matrix, vocab, cmatrix=None, cvocab=None):
         self.vocab = vocab
         self.matrix = matrix
         self.lookup = {v:i for i, v in enumerate(vocab)}
+
+        self.cmatrix = cmatrix
+        self.cvocab = cvocab
+        if cvocab is not None:
+            self.clookup = {v:i for i, v in enumerate(cvocab)}
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -17,6 +23,13 @@ class VectorSpace(object):
 
     def __contains__(self, key):
         return key in self.lookup
+
+    def save(self, filename):
+        if self.cmatrix is not None:
+            np.savez_compressed(filename, matrix=self.matrix, rows=self.vocab,
+                                cmatrix=self.cmatrix, crows=self.cvocab)
+        else:
+            np.savez_compressed(filename, matrix=self.matrix, rows=self.vocab)
 
     def save_mikolov_text(self, filename):
         with open(filename, 'w') as f:
@@ -33,7 +46,21 @@ class VectorSpace(object):
         return VectorSpace(newmatrix, newvocab)
 
     def normalize(self):
-        return VectorSpace(normalize(self.matrix, norm='l2', axis=1), self.vocab)
+        nmat = normalize(self.matrix, norm='l2', axis=1)
+        if self.cmatrix is not None:
+            cnmat = normalize(self.cmatrix, norm='l2', axis=1)
+        else:
+            cnmat = None
+        return VectorSpace(nmat, self.vocab, cnmat, self.cvocab)
+
+    def svd(self, k=0):
+        if not k:
+            k = self.matrix.shape[1]
+        U, s, V = np.linalg.svd(self.matrix, full_matrices=False)
+        s = np.sqrt(s)
+        U = U.dot(np.diag(s)).astype(np.float32)
+        # note we could do V here too...
+        return VectorSpace(U, self.vocab)
 
 def load_numpy(filename_combined, insertblank=False):
     data = np.load(filename_combined)
@@ -43,7 +70,20 @@ def load_numpy(filename_combined, insertblank=False):
         dims = mat.shape[1]
         mat = np.concatenate([np.zeros((1, dims), dtype=mat.dtype), mat])
         labels = np.concatenate([np.array(['']), labels])
-    return VectorSpace(mat, labels)
+
+    # some spaces have an additional context matrix
+    if 'crows' in data:
+        cmat = data['cmatrix']
+        clabels = data['crows']
+        if insertblank:
+            dims = cmat.shape[1]
+            cmat = np.concatenate([np.zeros((1, dims), dtype=cmat.dtype), cmat])
+            clabels = np.concatenate([np.array(['']), clabels])
+    else:
+        cmat = None
+        clabels = None
+
+    return VectorSpace(mat, labels, cmatrix=cmat, cvocab=clabels)
 
 def load_numpy_and_vocab(filename_matrix, filename_rows, insertblank=False):
     mat = np.load(filename_matrix)
@@ -55,19 +95,51 @@ def load_numpy_and_vocab(filename_matrix, filename_rows, insertblank=False):
         labels.insert(0, '')
     return VectorSpace(mat, labels)
 
-
 def load_mikolov_text(filename):
     vocab = []
-    matrix = []
     with open(filename) as f:
-        for l in f:
+        X, Y = f.readline().strip().split()
+        X, Y = int(X), int(Y)
+        matrix = np.zeros((X, Y))
+
+        for i, l in enumerate(f):
             l = l.strip().split()
             word = l.pop(0)
-            if word.endswith("-n"):
-                word = word[:-2]
             vocab.append(word)
-            matrix.append(np.array(map(float, l)))
+            matrix[i] = np.array(map(float, l))
 
-    matrix = np.array(matrix)
     return VectorSpace(matrix, vocab)
+
+def load_mikolov_binary(filename):
+    FLOAT_SIZE = 4
+    spacefile = open(filename)
+    header = spacefile.readline().rstrip()
+    vocab_s, dims = map(int, header.split(" "))
+
+    vocab = []
+
+    # init matrix
+    matrix = np.zeros((vocab_s, dims), dtype=np.float)
+
+    i = 0
+    while True:
+        line = spacefile.readline()
+        if not line:
+            break
+        sep = line.find(" ")
+        if sep == -1:
+            raise ValueError("Couldn't find the vocab/data separation character! Space file corruption?")
+
+        word = line[:sep]
+        data = line[sep+1:]
+        if len(data) < FLOAT_SIZE * dims + 1:
+            data += spacefile.read(FLOAT_SIZE * dims + 1 - len(data))
+        data = data[:-1]
+        vocab.append(word)
+        vector = (struct.unpack("%df" % dims, data))
+        matrix[i] = vector
+        i += 1
+
+    return VectorSpace(matrix, np.array(vocab))
+
 
